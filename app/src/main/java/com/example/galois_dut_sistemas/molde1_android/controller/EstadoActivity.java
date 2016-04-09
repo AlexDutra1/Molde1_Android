2dc84fe66841102d9a10b4da2e15aec457440991
AlexDutra1 <alexdf0001@gmail.com>
2016-04-09 14:34:59 -0300

    Estrutura do Projeto E Padrões de Projeto

---
+++ app/src/main/java/com/example/galois_dut_sistemas/molde1_android/controller/EstadoActivity.java
@@ -0,0 +1,9 @@
+package com.example.galois_dut_sistemas.molde1_android.controller;
+
+import android.app.Activity;
+
+/**
+ * Created by galois on 09/04/16.
+ */
+public class EstadoActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/galois_dut_sistemas/molde1_android/model/Estado.java
@@ -0,0 +1,35 @@
+package com.example.galois_dut_sistemas.molde1_android.model;
+
+/**
+ * Created by galois on 09/04/16.
+ */
+public class Estado {
+
+    private Long idEstado;
+    private String sigla;
+    private String nome;
+
+    public Long getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(Long idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
