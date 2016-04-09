30abc3b997063ba650f7cc17fd061849409af3dd
AlexDutra1 <alexdf0001@gmail.com>
2016-04-09 15:05:48 -0300

    Estrutura do Projeto E Padrões de Projeto

--- app/src/main/java/com/example/galois_dut_sistemas/molde1_android/controller/ClienteActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.galois_dut_sistemas.molde1_android.controller;
-
-import android.app.Activity;
-
-/**
- * Created by galois on 09/04/16.
- */
-public class ClienteActivity extends Activity {
-}
---
+++ app/src/main/java/com/example/galois_dut_sistemas/molde1_android/service_e_bo/EstadoServiceBO.java
@@ -0,0 +1,7 @@
+package com.example.galois_dut_sistemas.molde1_android.service_e_bo;
+
+/**
+ * Created by galois on 09/04/16.
+ */
+public class EstadoServiceBO {
+}
